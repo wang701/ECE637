00dc6fcc561461642e1708bd25d42dc2e5222cb3
Yang Wang <wang701@purdue.edu>
2016-01-20 21:35:13 -0500

    Add: working matlab code

--- image_filtering/source/matlab_code.m
+++ image_filtering/source/matlab_code.m
@@ -1,12 +1,61 @@
-u = -pi:0.001:pi;
-v = -pi:0.001:pi;
-H1 = 0;
-H2 = 0;
+u = -pi:0.1:pi;
+v = -pi:0.1:pi;
 
-for m = -4:4
-	H1 = H1 + exp((-i)*u*m);
+% Section 3 %
+[U,V] = meshgrid(u,v);
+H_lpf = (1/81)*sin(4.5*U).*sin(4.5*V)./(sin(0.5*U).*sin(0.5*V));
+fig1 = figure(1);
+surf(U,V,abs(H_lpf));
+xlabel('\mu');
+ylabel('\nu');
+zlabel('H(e^{j\mu},e^{j\nu})');
+print(fig1,'lpfcsft','-depsc','-tiff');
+
+% Section 4 %
+lamda = 1.5;
+H_lpf_1 = (1/25)*sin(2.5*U).*sin(2.5*V)./(sin(0.5*U).*sin(0.5*V));
+fig2 = figure(2);
+surf(U,V,abs(H_lpf_1));
+xlabel('\mu');
+ylabel('\nu');
+zlabel('H(e^{j\mu},e^{j\nu})');
+G = 1 + lamda*(1 - H_lpf_1);
+fig3 = figure(3);
+surf(U,V,abs(G));
+xlabel('\mu');
+ylabel('\nu');
+zlabel('G(e^{j\mu},e^{j\nu})');
+print(fig3,'hpfcsft','-depsc','-tiff');
+
+% Section 5 %
+H_iif = 0.01./(1-0.9*exp(-i*U)-0.9*exp(-i*V)+0.81*exp(-i*(U+V)));
+fig4 = figure(4);
+surf(U,V,abs(H_iif));
+xlabel('\mu');
+ylabel('\nu');
+zlabel('H(e^{j\mu},e^{j\nu})');
+print(fig4,'iircsft','-depsc','-tiff');
+
+x = zeros(256,256);
+y = zeros(256,256);
+x(128,128) = 1;
+
+for m = 1:256
+	for n = 1:256
+		y(m,n) = 0.01*x(m,n);
+		if (m > 1)
+			y(m,n) = y(m,n) + 0.9*y(m-1,n);
+		end
+		if (n > 1)
+			y(m,n) = y(m,n) + 0.9*y(m,n-1);
+		end
+		if (m > 1 && n > 1)
+			y(m,n) = y(m,n) - 0.81*y(m-1,n-1);
+		end
+	end
 end
-for n = -4:4
-	H2 = H2 + exp((-i)*v*n);
-end
-H = H1.*H2;
+
+y(y(:) > 255) = 255;
+y(y(:) < 0) = 0;
+
+imwrite(uint8(255*100*y),'h_out.tif');
