58e012e514863f2670dd6146024fe2f0c97733eb
Yang Wang <wang701@purdue.edu>
2016-02-02 01:04:59 -0500

    Add: completed source and imgs

--- neighborhoods_connected_components/report/segmen.c
+++ neighborhoods_connected_components/report/segmen.c
@@ -4,11 +4,13 @@ int main (int argc, char **argv) {
 	FILE *fp;
 	struct TIFF_img input_img;
 	struct pixel s;
-	int i, j, numcon;
-	int ClassLabel = 1;
+	int i, j, numcon, segLabel;
 	double T;
 
-	if (argc != 6) error(argv[0]);
+	if (argc != 4) error(argv[0]);
+
+	/* we have 1 segment at the beginning */
+	segLabel = 1;
 
 	/* open image file */
 	if ((fp = fopen(argv[1], "rb")) == NULL) {
@@ -32,37 +34,55 @@ int main (int argc, char **argv) {
 	}
 
 	/* get the other parameters */
-	sscanf(argv[2], "%d", &(s.m));
-	sscanf(argv[3], "%d", &(s.n));
-	sscanf(argv[4], "%lf", &T);
+	sscanf(argv[2], "%lf", &T);
 
 	unsigned int **seg = (unsigned int **)get_img(input_img.width,
                                                   input_img.height,
                                                   sizeof(unsigned int));
-	ConnectedSet(s, T, input_img.mono, input_img.width, input_img.height,
-                 ClassLabel, seg, &numcon);
 
+    /* make sure all elements are 0 */
+    for (i = 0; i < input_img.height; i++) {
+		for (j = 0; j < input_img.width; j++) {
+			seg[i][j] = 0;
+		}
+	}
+	
+    /* go through the image */
 	for (i = 0; i < input_img.height; i++) {
 		for (j = 0; j < input_img.width; j++) {
-			if (seg[i][j] == ClassLabel) {
-				input_img.mono[i][j] = 0;
-			} else {
-				input_img.mono[i][j] = 255;
+			if (seg[i][j] == 0) {
+				s.m = i;
+				s.n = j;
+				ConnectedSet(s, T, input_img.mono, input_img.width,
+                             input_img.height, segLabel, seg, &numcon);
+				if (numcon > 100) {
+                    /* new label created after 100 connected sets */
+					segLabel++;
+				} else {
+                    /* otherwise, back to 0 */
+					ConnectedSet(s, T, input_img.mono, input_img.width,
+                                 input_img.height, 0, seg, &numcon);
+				}
 			}
+		}
+	}
+	for (i = 0; i < input_img.height; i++) {
+		for (j = 0; j < input_img.width; j++) {
+			input_img.mono[i][j] = seg[i][j];
 		}
 	}
 
 	free_img((void *)seg);
 
 	/* open output image file */
-	if ((fp = fopen(argv[5], "wb")) == NULL) {
+	if ((fp = fopen(argv[3], "wb")) == NULL) {
 		fprintf(stderr, "cannot open file output.tif\n");
 		exit(1);
 	}
 
 	/* write output image */
 	if(write_TIFF(fp, &input_img)) {
-		fprintf(stderr, "error writing TIFF file %s\n", argv[5]);
+		fprintf(stderr, "error writing TIFF file %s\n", argv[3]);
 		exit(1);
 	}
 
