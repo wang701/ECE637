4fd46c8bb20a0dba15b07d649458a786f66aefb9
Yang Wang <wang701@purdue.edu>
2016-02-26 23:54:23 -0500

    Add: completed lab 5

--- eigen_decomposition/source/sec51.m
+++ eigen_decomposition/source/sec51.m
@@ -10,22 +10,22 @@ Z = (1/sqrt(n-1))*(bsxfun(@minus,X,mu_hat));
 A = U(:,1:10);
 Y = A'*((bsxfun(@minus,X,mu_hat)));
 
-Ck=26;
+Ck=10;
 empty_cell = cell(26,2);
 params = cell2struct(empty_cell,{'M','R'},2);
 
 for k = 1:1:26
-	mu_k_hat = sum(Y(:,1:Ck))/Ck;
+	mu_k_hat = mean(Y,2);
 	params(k).M = mu_k_hat;
-	Z_k = bsxfun(@minus,Y(:,1:Ck),mu_k_hat);
-	params(k).R = Z_k*Z_k'/(Ck-1);
+	Z_k = bsxfun(@minus,Y,mu_k_hat);
+	params(k).R = Z_k*Z_k'/(312-1);
 end
 
 read_test_data;
 TY = A'*((bsxfun(@minus,TX,mu_hat)));
 
 for k = 1:1:26
-	y_minus_mu_k = bsxfun(@minus,TY,params(k).M);
-	R_k = params(k).R;
+	y_minus_mu_k = bsxfun(@minus,TY,A'*params(k).M);
+	R_k = A'*params(k).R*A;
 	[k_min k_star] = min(y_minus_mu_k'*inv(R_k)*y_minus_mu_k + log(abs(R_k)));
 end
